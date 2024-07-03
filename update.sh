95e87401f196a4895900f687ddc3bfd6509674ff
TAKAMI Torao <koiroha@gmail.com>
2024-07-03 23:42:19 +0900

    kindle01のスクリーンサイズ変更

--- update.sh
+++ update.sh
@@ -10,7 +10,7 @@ cd /home/torao/git/doorstep-navi \
 && npm start \
 && convert png:output.png -colorspace lineargray -define png:color-type=0 -define png:bit-depth=8 output.png \
 && scp output.png root@hazm.at:/root/hazmat-contents/docroot/doorstep-navi/screen.png \
-&& cp output.png output01.png \
+&& convert png:output.png -resize 768x1024 output01.png \
 && convert png:output.png -resize 768x1024 output02.png \
 && for i in 1 2; \
   do scp output0$i.png kindle0$i:/mnt/us/ \
